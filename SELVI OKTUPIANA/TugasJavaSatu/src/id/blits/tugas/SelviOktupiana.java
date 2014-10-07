a7667c6a93c81cad7d7106b9ea5de9c6f50938ae
selvioktupiana <selvioktupiana@yahoo.com>
2014-10-08 02:15:31 +0700

    no message

---
+++ SELVI OKTUPIANA/TugasJavaSatu/src/id/blits/tugas/SelviOktupiana.java
@@ -0,0 +1,17 @@
+
+package id.blits.tugas;
+
+//Program Ini Dibuat Oleh, SELVI OKTUPIANA
+public class SelviOktupiana {
+
+    
+    public static void main(String[] args) {
+       String nama = "SELVI OKTUPIANA";
+        String npm = "13311359";
+        String kelas = "SI 13 E";
+        System.out.println("Perguruan Tinggi TEKNOKRAT");
+        System.out.println("Nama   : "+nama+"\n"+"NPM    : "+npm+"\n"+"Kelas  : "+kelas);
+     
+    }
+    
+}
